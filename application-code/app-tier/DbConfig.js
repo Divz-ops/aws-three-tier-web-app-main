5a38757c9b26db41fb616610819f2762b57bdc8f
Divz-ops <divyab667@gmail.com>
2025-05-23 17:33:28 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     DB_HOST : 'database-web.cziwc4k28qar.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'Admin123',
-    DB_DATABASE : 'Database-web'
+    DB_DATABASE : 'webappdb'
 });
