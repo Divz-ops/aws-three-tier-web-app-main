560a7d8e1bbc8a3e2be4188ca89721eaecb424e3
divya <divyab667@gmail.com>
2025-05-23 12:37:34 +0530

    DBConfig changes done

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'mysql',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin123',
+    DB_DATABASE : 'Database-web'
 });
